f040e9bd0f59d5033c657be27728b28ffb6a67b0
bw2016y <1556019082@qq.com>
2018-06-11 17:39:44 +0800

    06.11

---
+++ src/main/java/org/teamwe/carrent/bean/User.java
@@ -0,0 +1,10 @@
+package org.teamwe.carrent.bean;
+
+public class User {
+
+    public static int COMMEN_USER = 0;
+    public static int ENGENEER = 1;
+    public static int ADMINISTRATOR = 2;
+
+
+}
---
+++ src/main/java/org/teamwe/carrent/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package org.teamwe.carrent.dao;
+
+import org.teamwe.carrent.bean.User;
+
+public interface UserDAO {
+
+     boolean Add_usr(User user);
+     User Get_userByEmial(String email);
+     boolean Update_user (User user);
+     boolean Delete_user(String email);
+
+
+
+
+}
