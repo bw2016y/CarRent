fa2247b3a29e1ed57b635b2652a396bba635ff8a
bw2016y <1556019082@qq.com>
2018-06-13 10:34:14 +0800

    6.13.2

---
+++ src/main/java/org/teamwe/carrent/dao/CarimgDAO.java
@@ -0,0 +1,27 @@
+package org.teamwe.carrent.dao;
+
+import org.teamwe.carrent.entity.CarImg;
+
+import java.util.List;
+
+public interface CarimgDAO {
+
+
+    /**
+     *        插入一条carImg记录
+     * @return
+     */
+    public int sava_img(CarImg carImg);
+
+
+    /**
+     *  根据一个card获取所有该车辆img hash
+     * @param card
+     * @return
+     */
+    public List<CarImg>  get_cardimg_by_card(String card);
+
+
+    
+
+}
