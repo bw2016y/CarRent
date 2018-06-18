4e091d5898042409051aac8f159f0a861e244539
fdws <885374150@qq.com>
2018-06-18 14:30:41 +0800

    Add Global Controller to deal with exception

---
+++ src/main/java/org/teamwe/carrent/controller/GlobalController.java
@@ -0,0 +1,9 @@
+package org.teamwe.carrent.controller;
+
+/**
+ * @author FDws
+ * Created on 2018/6/18 14:02
+ */
+
+public class GlobalController {
+}
---
+++ src/main/java/org/teamwe/carrent/controller/utils/SessionAttr.java
@@ -0,0 +1,13 @@
+package org.teamwe.carrent.controller.utils;
+
+import org.teamwe.carrent.entity.User;
+
+/**
+ * @author FDws
+ * Created on 2018/6/18 13:57
+ */
+
+public class SessionParam {
+    public static String USER_ID = User.class.getName();
+    public static String USER_TYPE = USER_ID + "_TYPE";
+}
