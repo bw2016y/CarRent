49843fde6fcc6f97eb042b857824d2ba5971fb22
fdws <885374150@qq.com>
2018-06-18 13:34:59 +0800

    Complete Order Controller

---
+++ src/main/java/org/teamwe/carrent/controller/OrderController.java
@@ -0,0 +1,13 @@
+package org.teamwe.carrent.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author FDws
+ * Created on 2018/6/17 21:06
+ */
+
+@RestController
+public class OrderController {
+    // TODO Order Function
+}
