81611709203ea9123cb6a880269e632ae45f821f
FDws <885374150@qq.com>
2018-06-20 08:38:28 +0800

    Add new url

--- src/main/java/org/teamwe/carrent/controller/LoginController.java
+++ src/main/java/org/teamwe/carrent/controller/LoginController.java
@@ -1,5 +1,6 @@
 package org.teamwe.carrent.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.teamwe.carrent.controller.utils.Format;
