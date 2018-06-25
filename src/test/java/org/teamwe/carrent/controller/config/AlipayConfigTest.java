56b1206e3c2b604987499a50286736fcefeec56f
FDws <885374150@qq.com>
2018-06-25 08:43:09 +0800

    Change Test File

--- src/test/java/org/teamwe/carrent/controller/config/AlipayConfigTest.java
+++ src/test/java/org/teamwe/carrent/controller/config/AlipayConfigTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.teamwe.carrent.CarRentApplication;
 
@@ -21,8 +22,15 @@ public class AlipayConfigTest {
     @Value("${project.controller.domain}")
     private String url;
 
+    @Value("#{'${project.controller.allowed.origins}'.split(', *')}")
+    private String[] domainFront;
+
+    @Autowired
+    private Environment env;
+
     @Test
-    public void test(){
+    public void test() {
+        System.out.println(url + ":" + env.getProperty("local.server.port"));
         System.out.println(url);
     }
 }
