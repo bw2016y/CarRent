f627fcaac9f3bead89a8955f48fd78aa37c922a5
FDws <885374150@qq.com>
2018-06-27 14:45:15 +0800

    Delete error url

--- src/main/java/org/teamwe/carrent/controller/GlobalController.java
+++ src/main/java/org/teamwe/carrent/controller/GlobalController.java
@@ -3,7 +3,6 @@ package org.teamwe.carrent.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.teamwe.carrent.controller.utils.Format;
 import org.teamwe.carrent.utils.ReturnStatus;
@@ -73,9 +72,5 @@ public class GlobalController {
             sb.append("]");
             log.error(sb.toString());
         }
-    }
-    @RequestMapping("/")
-    public Format error(){
-        return new Format().code(ReturnStatus.SUCCESS).message("Error Message");
     }
 }
