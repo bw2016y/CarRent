8eb39c5a426ec9ad2ecdd0489a3eb85b57b62a43
SevenAMan <39973102+SevenAMan@users.noreply.github.com>
2018-07-09 19:15:46 +0800

    delete unused param

--- src/main/java/org/teamwe/carrent/controller/config/AlipayConfig.java
+++ src/main/java/org/teamwe/carrent/controller/config/AlipayConfig.java
@@ -5,7 +5,6 @@ import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayTradePagePayRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
